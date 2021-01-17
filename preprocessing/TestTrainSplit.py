3c4195d001ef1ac643e036b40cf83fca55987217
Markus Laubenthal <mail@markuslaubenthal.de>
2021-01-17 01:41:33 +0100

    fix

--- preprocessing/TestTrainSplit.py
+++ preprocessing/TestTrainSplit.py
@@ -2,7 +2,7 @@
 
 
 def seven_days_train_test_split(dataset, max_lookback):
-  train_indices = range(0,X_closeness.shape[0] - max_lookback)
-  test_indices = range(X_closeness.shape[0] - max_lookback, X_closeness.shape[0])
+  train_indices = range(0,dataset.shape[0] - max_lookback)
+  test_indices = range(dataset.shape[0] - max_lookback, dataset.shape[0])
   dataset_train, dataset_test = dataset[train_indices], dataset[test_indices]
   return dataset_train, dataset_test
