1d527a463df1214851769f63973c3438e878ee92
Lennard Alms <mail@lennardalms.de>
2021-01-18 16:57:10 +0100

    preprocessing fix

--- Postprocessing/ErrorEvaluation.py
+++ Postprocessing/ErrorEvaluation.py
@@ -1,4 +1,4 @@
-def calculate_errors(pred_in, base_in, max_lookback=168, test_size=168):
+def calculate_errors(pred_in, base_in, internet_min, internet_max, max_lookback=168, test_size=168):
     pred = pred_in.copy()
     pred = pred.reshape((pred.shape[0],10000)).T
     pred = pred * (internet_max - internet_min)[:,np.newaxis] + internet_min[:,np.newaxis]
