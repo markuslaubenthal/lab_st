741b912b7b6f9c093adfb53e309e000a8610646a
Markus Laubenthal <mail@markuslaubenthal.de>
2021-01-18 15:00:11 +0100

    Time Embedding

--- model/SplitDenseNetFactory.py
+++ model/SplitDenseNetFactory.py
@@ -1,4 +1,5 @@
 from .DenseNetFactory import DenseNetFactory
+from .TimeEmbeddingFactory import TimeEmbeddingFactory
 import tensorflow as tf
 from tensorflow.keras import layers
 class SplitDenseNetFactory():
@@ -6,14 +7,17 @@ class SplitDenseNetFactory():
         self.x = 0
 
 
-    def Model(self, closeness_length, period_length):
+    def Model(self, closeness_length=3, period_length=3, time_shape=(24+7+1,)):
         dn_factory = DenseNetFactory()
+        te_factory = TimeEmbeddingFactory()
         period_dependency_model, period_input = dn_factory.Model(prefix="period_dependency", input_shape=(100,100,period_length))
         closeness_dependency_model, closeness_input = dn_factory.Model(prefix="closeness_dependency", input_shape=(100,100,closeness_length))
 
-        combined = layers.Add()([period_dependency_model, closeness_dependency_model])
+        time_model, time_input = te_factory.Model(input_shape=time_shape)
+
+        combined = layers.Add()([period_dependency_model, closeness_dependency_model, time_model])
         combined = layers.Activation('sigmoid', name="output_sigmoid")(combined)
 
         combined = layers.Flatten()(combined)
-        model = tf.keras.models.Model([period_input, closeness_input], combined)
+        model = tf.keras.models.Model([period_input, closeness_input, time_input], combined)
         return model
---
+++ test.py
@@ -0,0 +1,4 @@
+from model.SplitDenseNetFactory import SplitDenseNetFactory
+
+dn = SplitDenseNetFactory()
+model = dn.Model()
