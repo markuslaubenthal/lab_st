ab0e353b5f74d17cbd06cae91d6f279757fd6363
Markus Laubenthal <mail@markuslaubenthal.de>
2021-01-26 14:57:58 +0100

    added multiple time embedding methods

--- model/SplitDenseNetFactory.py
+++ model/SplitDenseNetFactory.py
@@ -7,7 +7,7 @@ class SplitDenseNetFactory():
         self.x = 0
 
 
-    def Model(self, closeness_length=3, period_length=3, time_shape=(24+7+1,)):
+    def Model(self, closeness_length=3, period_length=3, time_shape=(24+7+1,), time_embedding_method=0):
         dn_factory = DenseNetFactory()
         te_factory = TimeEmbeddingFactory()
         period_dependency_model, period_input = dn_factory.Model(prefix="period_dependency", input_shape=(100,100,period_length))
@@ -16,9 +16,25 @@ class SplitDenseNetFactory():
         time_model, time_input = te_factory.Model(input_shape=time_shape)
 
         combined = layers.Add()([period_dependency_model, closeness_dependency_model])
-        combined = layers.Add()([combined, time_model])
+        combined = self.TimeEmbeddingMethod(time_model, combined, method=time_embedding_method)
+
         combined = layers.Activation('sigmoid', name="output_sigmoid")(combined)
 
         combined = layers.Flatten()(combined)
         model = tf.keras.models.Model([period_input, closeness_input, time_input], combined)
         return model
+
+    def TimeEmbeddingMethod(self, time_output, model, method = 0):
+        # Simple Add
+        if method == 0:
+            return layers.Add()([model, time_output])
+        # Hadamard scale
+        if method == 1:
+            time_model = HadamardLayer()(time_output)
+            model = layers.Multiply()([model, time_model])
+            return model
+        # Scale from 0 - 2
+        if method == 2:
+            time_model = layers.Lambda(lambda x: x*2)(time_output)
+            model = layers.Multiply()([model, time_model])
+            return model
