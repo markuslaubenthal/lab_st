3661c4ce43045e086cf53b57b64116efd632e65c
Markus Laubenthal <mail@markuslaubenthal.de>
2021-01-26 17:33:47 +0100

    added another time embedding method

--- model/SplitDenseNetFactory.py
+++ model/SplitDenseNetFactory.py
@@ -7,34 +7,61 @@ class SplitDenseNetFactory():
         self.x = 0
 
 
-    def Model(self, closeness_length=3, period_length=3, time_shape=(24+7+1,), time_embedding_method=0):
+    def Model(self, closeness_length=3, period_length=3, time_shape=(24+7+1,), time_embedding_method=None, t_minus_one=False):
         dn_factory = DenseNetFactory()
         te_factory = TimeEmbeddingFactory()
+
+        # if time_embedding_method == "in_front":
+        #     time_model, time_input = te_factory.Model(input_shape=time_shape)
+        #     time_embedding = self.TimeEmbeddingMethod(time_model, combined, method=time_embedding_method)
+
+
+
+
+
         period_dependency_model, period_input = dn_factory.Model(prefix="period_dependency", input_shape=(100,100,period_length))
         closeness_dependency_model, closeness_input = dn_factory.Model(prefix="closeness_dependency", input_shape=(100,100,closeness_length))
+        inputs = [period_input, closeness_input]
 
-        time_model, time_input = te_factory.Model(input_shape=time_shape)
+        t_m1_input = None
+        if t_minus_one:
+            t_m1_input = keras.Input(name="t_minus_1_input", input_shape=(100,100,1))
+            inputs.append(t_m1_input)
 
         combined = layers.Add()([period_dependency_model, closeness_dependency_model])
-        combined = self.TimeEmbeddingMethod(time_model, combined, method=time_embedding_method)
+
+        if time_embedding_method is not None:
+            time_model, time_input = te_factory.Model(input_shape=time_shape)
+            combined = self.TimeEmbeddingMethod(time_model, combined, method=time_embedding_method, t_m1_input=t_m1_input)
+            inputs.append(time_input)
 
         combined = layers.Activation('sigmoid', name="output_sigmoid")(combined)
-
         combined = layers.Flatten()(combined)
-        model = tf.keras.models.Model([period_input, closeness_input, time_input], combined)
+
+
+
+        model = tf.keras.models.Model(inputs, combined)
         return model
 
-    def TimeEmbeddingMethod(self, time_output, model, method = 0):
+    def TimeEmbeddingMethod(self, time_output, model, method = "add", t_m1_input=None):
         # Simple Add
-        if method == 0:
+        if method == "add":
             return layers.Add()([model, time_output])
         # Hadamard scale
-        if method == 1:
+        if method == "weighted multiply":
             time_model = HadamardLayer()(time_output)
             model = layers.Multiply()([model, time_model])
             return model
         # Scale from 0 - 2
-        if method == 2:
+        if method == "scale_0_2":
             time_model = layers.Lambda(lambda x: x*2)(time_output)
             model = layers.Multiply()([model, time_model])
+            return model
+        if method == 't_minus_one':
+            if t_m1_input is None:
+                print("t_m1_input can not be None")
+            time_model = layers.Multiply()([time_output, t_m1_input])
+            model = layers.Concatenate(axis=3)([time_model, model])
+            model = HadamardLayer()(model)
+            model = layers.Conv1D(1,1)(model)
             return model
