c5720eb36cf53c074cf5347243de95407d36fc6f
Markus Laubenthal <mail@markuslaubenthal.de>
2021-01-28 21:53:48 +0100

    Redesign

--- model/DenseNetFactory.py
+++ model/DenseNetFactory.py
@@ -59,7 +59,7 @@ class DenseNetFactory():
             else:
                 concatenationLayer = layers.Concatenate(axis=self.concat_axis)([x, concatenationLayer])
                 x = concatenationLayer
-        model = layers.Concatenate(axis=self.concat_axis)([input, x])
+        # model = layers.Concatenate(axis=self.concat_axis)([input, x])
         model = layers.Conv1D(1,1, use_bias=False)(model) #Add activation Layer
         model = layers.Activation('relu')(model)
         model = HadamardLayer(name = prefix + "_hadamard1")(model)
